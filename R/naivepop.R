815e69b94fd3ea9e34c71f64957dc5688fb2323b
Mar-vazquez <112626468+Mar-vazquez@users.noreply.github.com>
2023-01-19 15:07:29 +0100

    add naivepop function, survival and stats import (#6)
    closes #5
    
    Co-authored-by: Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>

---
+++ R/naivepop.R
@@ -0,0 +1,41 @@
+#' Naive Overall Population Model Estimation
+#'
+#' Function to fit the naive overall population model to the data. This model is going to
+#' estimate the subgroup treatment effects with the overall population
+#' treatment effect.
+#'
+#' @param resp (`string`)\cr the response variable name.
+#' @param trt (`string`)\cr the treatment variable name.
+#' @param data (`data frame`)\cr the data frame with the variables.
+#' @param resptype (`string`)\cr the type of data used. Can be "survival"
+#'   or "binary".
+#' @param status (`string`)\cr only for "survival" `resptype`,
+#'   the status variable name in survival data.
+#'
+#' @return List with `fit`, `model`, `resptype`, `data`.
+#' @export
+#'
+#' @examples
+#' naivepop("futime", "trt", survival::myeloid, "survival", "death")
+naivepop <- function(resp, trt, data, resptype = c("survival", "binary"), status = NULL){
+  assert_string(resp)
+  assert_string(trt)
+  assert_data_frame(data)
+  resptype <- match.arg(resptype)
+
+  fit_pop <- if (resptype == "survival"){
+    assert_string(status)
+    form_surv <- stats::as.formula(paste("survival::Surv(", resp, ",", status, ") ~ ", trt))
+    survival::coxph(formula = form_surv, data = data)
+  } else if (resptype == "binary"){
+    form_bin <- stats::as.formula(paste(resp, " ~ ", trt))
+    stats::glm(formula = form_bin, data = data, family = "binomial")
+  }
+
+  list(
+    fit = fit_pop,
+    model = "naive_pop",
+    resptype = resptype,
+    data = data
+  )
+}
--- R/shrinkforest-package.R
+++ R/shrinkforest-package.R
@@ -2,4 +2,6 @@
 "_PACKAGE"
 
 #' @import checkmate
+#' @importFrom survival aareg
+#' @importFrom stats acf
 NULL
