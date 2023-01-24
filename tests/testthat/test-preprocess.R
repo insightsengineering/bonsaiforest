cc45ce54868f4de0f86eacaf907b510a266a6020
Mar-vazquez <112626468+Mar-vazquez@users.noreply.github.com>
2023-01-24 17:19:41 +0100

    Add preprocessing_data (#17)
    closes #16
    
    Signed-off-by: Mar-vazquez <112626468+Mar-vazquez@users.noreply.github.com>
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

---
+++ tests/testthat/test-preprocess.R
@@ -0,0 +1,17 @@
+test_that("Preprocess outputs the right elements", {
+  result <- preprocess("arm", c("x_1", "x_3"), c("x_1", "x_2", "x_3"),
+                               example_data)
+  design_main <- stats::model.matrix(~ -1 + arm + x_1 + x_2 + x_3,
+                                     data = example_data)
+  design_dummy <- cbind(stats::model.matrix(~ -1 + x_1, data = example_data),
+                        stats::model.matrix(~ -1 + x_2, data = example_data),
+                        stats::model.matrix(~ -1 + x_3, data = example_data))
+  design_ia <- design_dummy * as.numeric(example_data$arm == 1)
+  colnames(design_ia) <- paste(colnames(design_ia), "arm", sep = "_")
+  subgr_names <- c("x_1a", "x_1b", "x_3a", "x_3b")
+  expected <- list(design_ia = design_ia,
+                   design_main = design_main,
+                   design_dummy = design_dummy,
+                   subgr_names = subgr_names)
+  expect_equal(result, expected)
+})
