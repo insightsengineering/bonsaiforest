e5976b9c4d039df9bf401b21b15b44826cdb4110
Mar-vazquez <112626468+Mar-vazquez@users.noreply.github.com>
2023-01-25 14:33:49 +0100

    Add elastic_net function (#19)
    closes #18

---
+++ R/elastic_net.R
@@ -0,0 +1,72 @@
+#' Elastic Net Penalization Model Estimation
+#'
+#' Function to fit the elastic net penalization model to the data. This model
+#' penalizes the interaction between the covariates and the treatment but
+#' leaves unpenalized the main effects.
+#'
+#' @param resp (`string`)\cr the response variable name.
+#' @param trt (`string`)\cr the treatment variable name.
+#' @param subgr (`character`)\cr vector with the name of the subgroup variables
+#'  from which we want to obtain the subgroup treatment effect.
+#' @param covars (`character`)\cr vector with the name of the variables that
+#'  we want to include in the model. The `subgr` variables have to be included
+#'  here.
+#' @param data (`data frame`)\cr the data frame with the variables.
+#' @param resptype (`string`)\cr the type of data used. Can be "survival"
+#'  or "binary".
+#' @param alpha (`scalar`)\cr the elastic net mixing parameter with values
+#'  between 0 and 1. The special case of `alpha`=1 corresponds to a lasso
+#'  penalty and the case of `alpha`=0 to a ridge penalty.
+#' @param status (`string`)\cr only for "survival" `resptype`,
+#'  the status variable name in survival data.
+#'
+#' @return List with `fit`, `model`, `resptype`, `data`, `alpha`,
+#'  `design_matrix`, `design_dummy`, `y`, `subgr_names`.
+#' @export
+#'
+#' @examples
+#' elastic_net("tt_pfs", "arm", c("x_1", "x_2"), c("x_1", "x_2", "x_3"),
+#'  example_data, "survival", 1, "ev_pfs")
+elastic_net <- function(resp, trt, subgr, covars, data,
+                        resptype = c("survival", "binary"), alpha,
+                        status = NULL) {
+  assert_string(resp)
+  assert_string(trt)
+  assert_character(subgr)
+  assert_character(covars)
+  assert_data_frame(data)
+  assert_scalar(alpha)
+  resptype <- match.arg(resptype)
+  prep_data <- preprocess(trt, subgr, covars, data)
+  if (resptype == "survival") {
+    assert_string(status)
+    penalty_factor <- c(rep(0, ncol(prep_data$design_main)),
+                        rep(1, ncol(prep_data$design_ia)))
+    design_matrix <- cbind(prep_data$design_main, prep_data$design_ia)
+    fit_glmnet <- glmnet::cv.glmnet(design_matrix,
+                                    survival::Surv(data[[resp]], data[[status]]),
+                                    family = "cox",
+                                    penalty.factor = penalty_factor,
+                                    alpha = alpha)
+    y <- as.data.frame(cbind(data[[resp]], data[[status]]))
+    colnames(y) <- c("resp", "status")
+  } else if (resptype == "binary") {
+    penalty_factor <- c(rep(0, ncol(prep_data$design_main[, -1])),
+                        rep(1, ncol(prep_data$design_ia)))
+    design_matrix <- cbind(prep_data$design_main[, -1], prep_data$design_ia)
+    fit_glmnet <- glmnet::cv.glmnet(design_matrix, data[[resp]],
+                                    family = "binomial",
+                                    penalty.factor = penalty_factor,
+                                    alpha = alpha)
+    y <- data[[resp]]
+  }
+  list(fit = fit_glmnet,
+       model = "elastic_net",
+       resptype = resptype,
+       data = data,
+       alpha = alpha,
+       design_matrix = design_matrix,
+       design_dummy = prep_data$design_dummy,
+       y = y,
+       subgr_names = prep_data$subgr_names)
+}
---
+++ tests/testthat/test-elastic_net.R
@@ -0,0 +1,60 @@
+test_that("Elastic_net outputs the right elements for survival", {
+  set.seed(0)
+  result <- elastic_net("tt_pfs", "arm", c("x_1", "x_3"),
+                        c("x_1", "x_2", "x_3"), example_data,
+                        "survival", 1, "ev_pfs")
+  result[[1]] <- coef(result$fit)
+  prep_data <- preprocess("arm", c("x_1", "x_3"), c("x_1", "x_2", "x_3"),
+                          example_data)
+  penalty_factor <- c(rep(0, 5), rep(1, 6))
+  design_matrix <- cbind(prep_data$design_main, prep_data$design_ia)
+  set.seed(0)
+  fit_glmnet <- glmnet::cv.glmnet(design_matrix,
+                                  survival::Surv(example_data$tt_pfs,
+                                                 example_data$ev_pfs),
+                                  family = "cox",
+                                  penalty.factor = penalty_factor,
+                                  alpha = 1)
+  y <- as.data.frame(cbind(example_data$tt_pfs, example_data$ev_pfs))
+  colnames(y) <- c("resp", "status")
+  expected <- list(fit = coef(fit_glmnet),
+                   model = "elastic_net",
+                   resptype = "survival",
+                   data = example_data,
+                   alpha = 1,
+                   design_matrix = design_matrix,
+                   design_dummy = prep_data$design_dummy,
+                   y = y,
+                   subgr_names = prep_data$subgr_names)
+  expect_equal(result, expected)
+})
+
+
+test_that("Elastic_net outputs the right elements for binary", {
+  set.seed(0)
+  result <- elastic_net("ev_pfs", "arm", c("x_1", "x_3"),
+                        c("x_1", "x_2", "x_3"), example_data,
+                        "binary", 0)
+  result[[1]] <- coef(result$fit)
+  prep_data <- preprocess("arm", c("x_1", "x_3"), c("x_1", "x_2", "x_3"),
+                          example_data)
+  penalty_factor <- c(rep(0, 4), rep(1, 6))
+  design_matrix <- cbind(prep_data$design_main[, -1], prep_data$design_ia)
+  set.seed(0)
+  fit_glmnet <- glmnet::cv.glmnet(design_matrix,
+                                  example_data$ev_pfs,
+                                  family = "binomial",
+                                  penalty.factor = penalty_factor,
+                                  alpha = 0)
+  y <- example_data$ev_pfs
+  expected <- list(fit = coef(fit_glmnet),
+                   model = "elastic_net",
+                   resptype = "binary",
+                   data = example_data,
+                   alpha = 0,
+                   design_matrix = design_matrix,
+                   design_dummy = prep_data$design_dummy,
+                   y = y,
+                   subgr_names = prep_data$subgr_names)
+  expect_equal(result, expected)
+})
