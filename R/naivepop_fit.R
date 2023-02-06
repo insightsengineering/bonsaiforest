3609423dfda1636e491efdaa664f9a31f2ed8f53
Mar-vazquez <112626468+Mar-vazquez@users.noreply.github.com>
2023-02-06 12:08:48 +0100

    Add naivepop_fit (#25)
    Closes #24

---
+++ R/naivepop_fit.R
@@ -0,0 +1,9 @@
+#' Naivepop fit
+#'
+#' A fitted model with the naivepop function.
+#'
+#'
+#' @format
+#' A `naivepop` object fitted with `example_data`.
+#' @source This is an artificial example.
+"naivepop_fit"
