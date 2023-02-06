08394873e1f03bf04b52f392af7b938005d1df95
Mar <mar.vazquez_rabunal@roche.com>
2023-02-06 17:02:48 +0100

    Add summary.naivepop

---
+++ R/summary.R
@@ -0,0 +1,22 @@
+#' Summary Naivepop Function
+#'
+#' Function to obtain the overall subgroup treatment effect of a
+#' naivepop object.
+#' @param object (`naivepop`)\cr the naivepop object.
+#' @param ... \cr arguments of summary.
+#'
+#' @return The estimated overall treatment effects.
+#' @export
+#'
+#' @examples
+#' summary(naivepop_fit_surv)
+summary.naivepop <- function(object, ...) {
+  assert_class(object, c("shrinkforest", "naivepop"))
+  trt_effect <- if (object$resptype == "survival"){
+    exp(stats::coef(object$fit))
+  } else if (object$resptype == "binary"){
+    stats::coef(object$fit)[2]
+  }
+  return(trt_effect)
+}
+
