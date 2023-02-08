dd3959509f7c021943a5e8804d1e6bdca5e657c8
Mar <mar.vazquez_rabunal@roche.com>
2023-02-08 17:31:38 +0100

    Add ahr_estimation

--- R/elastic_net_surv.R
+++ R/elastic_net_surv.R
@@ -1,8 +1,8 @@
-#' H0, Coefficients Elastic Net Survival Model and Design Matrix
+#' H0, Coefficients Elastic Net Survival Model and Matrices
 #'
 #' List with the cumulative baseline hazard, with the estimated coefficients
-#' of the model fitted with an elastic net and survival data and with the
-#' design matrix.
+#' of the model fitted with an elastic net and survival data, with the general
+#' design matrix and with the design and dummy matrices for subgroup x_1a.
 #'
 #'
 #' @format
@@ -10,5 +10,7 @@
 #'   - `h0`: the cumulative baseline hazard.
 #'   - `est_coef`: the estimated coefficients of the fitted model.
 #'   - `x`: the design matrix of the model.
+#'   - `dummy1`: the dummy matrix of the model for subgroup x_1a.
+#'   - `design1`: the design matrix of the model for subgroup x_1a.
 #' @source This is an artificial example.
 "elastic_net_surv"
