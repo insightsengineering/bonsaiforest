74918b58cd2cf154f22c43f0ed873987ca8d2dec
Mar <mar.vazquez_rabunal@roche.com>
2023-02-15 14:34:52 +0100

    Separate common and different parts

--- R/plot.R
+++ R/plot.R
@@ -14,58 +14,35 @@ plot.summary.naive <- function(x, ...) {
   assert_class(x, "summary.naive")
   data <- data.frame(x$estimates, model = rep("Naive", nrow(x$estimates)))
   data$subgroup <- as.factor(data$subgroup)
-  if (x$resptype == "survival") {
-    forestplot <- ggplot(
-      data = data,
-      aes(x = trt.estimate, y = model, xmin = trt.low, xmax = trt.high)
+  forestplot <- ggplot(
+    data = data,
+    aes(x = trt.estimate, y = model, xmin = trt.low, xmax = trt.high)
+  ) +
+    ggtitle("Forest plot Naive") +
+    geom_pointrange(aes(col = model)) +
+    ylab("Subgroup") +
+    geom_errorbar(aes(xmin = trt.low, xmax = trt.high, col = model), width = 0.5, cex = 1) +
+    facet_wrap(~ forcats::fct_inorder(subgroup),
+               strip.position = "left",
+               nrow = nrow(data), scales = "free_y"
     ) +
-      ggtitle("Forest plot Naive") +
-      geom_pointrange(aes(col = model)) +
-      geom_vline(xintercept = 1, linetype = 2) +
-      ylab("Subgroup") +
-      xlab("Hazard ratio") +
-      geom_errorbar(aes(xmin = trt.low, xmax = trt.high, col = model), width = 0.5, cex = 1) +
-      facet_wrap(~ forcats::fct_inorder(subgroup),
-        strip.position = "left",
-        nrow = nrow(data), scales = "free_y"
-      ) +
-      theme(
-        plot.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
-        strip.text.y.left = element_text(angle = 0)
-      ) +
-      scale_colour_discrete("Model")
+    theme(
+      plot.title = element_text(size = 16, face = "bold"),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.text.x = element_text(face = "bold"),
+      axis.title = element_text(size = 12, face = "bold"),
+      strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
+      strip.text.y.left = element_text(angle = 0)
+    ) +
+    scale_colour_discrete("Model")
+  if(x$resptype == "survival") {
+    forestplot + xlab("Hazard ratio") +
+    geom_vline(xintercept = 1, linetype = 2)
   } else if (x$resptype == "binary") {
-    forestplot <- ggplot(
-      data = data,
-      aes(x = trt.estimate, y = model, xmin = trt.low, xmax = trt.high)
-    ) +
-      ggtitle("Forest plot Naive") +
-      geom_pointrange(aes(col = model)) +
-      geom_vline(xintercept = 0, linetype = 2) +
-      ylab("Subgroup") +
-      xlab("Log Odds-Ratio") +
-      geom_errorbar(aes(xmin = trt.low, xmax = trt.high, col = model), width = 0.5, cex = 1) +
-      facet_wrap(~ forcats::fct_inorder(subgroup),
-        strip.position = "left",
-        nrow = nrow(data), scales = "free_y"
-      ) +
-      theme(
-        plot.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
-        strip.text.y.left = element_text(angle = 0)
-      ) +
-      scale_colour_discrete("Model")
+    forestplot + xlab("Log Odds-Ratio") +
+    geom_vline(xintercept = 0, linetype = 2)
   }
-  forestplot
 }
 
 
@@ -91,56 +68,34 @@ plot.summary.elastic_net <- function(x, ...) {
     data <- data.frame(x$estimates, model = rep("Elastic net", nrow(x$estimates)))
   }
   data$subgroup <- as.factor(data$subgroup)
+  forestplot <- ggplot(
+    data = data,
+    aes(x = trt.estimate, y = model)
+  ) +
+    ggtitle("Forest plot Elastic Net") +
+    geom_point(aes(col = model)) +
+    ylab("Subgroup") +
+    facet_wrap(~ forcats::fct_inorder(subgroup),
+               strip.position = "left",
+               nrow = nrow(data), scales = "free_y"
+    ) +
+    theme(
+      plot.title = element_text(size = 16, face = "bold"),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.text.x = element_text(face = "bold"),
+      axis.title = element_text(size = 12, face = "bold"),
+      strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
+      strip.text.y.left = element_text(angle = 0)
+    ) +
+    scale_colour_discrete("Model")
   if (x$resptype == "survival") {
-    forestplot <- ggplot(
-      data = data,
-      aes(x = trt.estimate, y = model)
-    ) +
-      ggtitle("Forest plot Elastic Net") +
-      geom_point(aes(col = model)) +
-      geom_vline(xintercept = 1, linetype = 2) +
-      ylab("Subgroup") +
-      xlab("Hazard ratio") +
-      facet_wrap(~ forcats::fct_inorder(subgroup),
-        strip.position = "left",
-        nrow = nrow(data), scales = "free_y"
-      ) +
-      theme(
-        plot.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
-        strip.text.y.left = element_text(angle = 0)
-      ) +
-      scale_colour_discrete("Model")
+    forestplot + xlab("Hazard ratio") +
+      geom_vline(xintercept = 1, linetype = 2)
   } else if (x$resptype == "binary") {
-    forestplot <- ggplot(
-      data = data,
-      aes(x = trt.estimate, y = model)
-    ) +
-      ggtitle("Forest plot Elastic Net") +
-      geom_point(aes(col = model)) +
-      geom_vline(xintercept = 0, linetype = 2) +
-      ylab("Subgroup") +
-      xlab("Log Odds-Ratio") +
-      facet_wrap(~ forcats::fct_inorder(subgroup),
-        strip.position = "left",
-        nrow = nrow(data), scales = "free_y"
-      ) +
-      theme(
-        plot.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
-        strip.text.y.left = element_text(angle = 0)
-      ) +
-      scale_colour_discrete("Model")
+    forestplot + xlab("Log Odds-Ratio") +
+      geom_vline(xintercept = 0, linetype = 2)
   }
-  forestplot
 }
 
 
@@ -160,56 +115,33 @@ plot.summary.horseshoe <- function(x, ...) {
   assert_class(x, "summary.horseshoe")
   data <- data.frame(x$estimates, model = rep("Horseshoe", nrow(x$estimates)))
   data$subgroup <- as.factor(data$subgroup)
-  if (x$resptype == "survival") {
-    forestplot <- ggplot(
-      data = data,
-      aes(x = trt.estimate, y = model, xmin = trt.low, xmax = trt.high)
+  forestplot <- ggplot(
+    data = data,
+    aes(x = trt.estimate, y = model, xmin = trt.low, xmax = trt.high)
+  ) +
+    ggtitle("Forest plot Horseshoe") +
+    geom_pointrange(aes(col = model)) +
+    ylab("Subgroup") +
+    geom_errorbar(aes(xmin = trt.low, xmax = trt.high, col = model), width = 0.5, cex = 1) +
+    facet_wrap(~ forcats::fct_inorder(subgroup),
+               strip.position = "left",
+               nrow = nrow(data), scales = "free_y"
     ) +
-      ggtitle("Forest plot Horseshoe") +
-      geom_pointrange(aes(col = model)) +
-      geom_vline(xintercept = 1, linetype = 2) +
-      ylab("Subgroup") +
-      xlab("Hazard ratio") +
-      geom_errorbar(aes(xmin = trt.low, xmax = trt.high, col = model), width = 0.5, cex = 1) +
-      facet_wrap(~ forcats::fct_inorder(subgroup),
-        strip.position = "left",
-        nrow = nrow(data), scales = "free_y"
-      ) +
-      theme(
-        plot.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
-        strip.text.y.left = element_text(angle = 0)
-      ) +
-      scale_colour_discrete("Model")
+    theme(
+      plot.title = element_text(size = 16, face = "bold"),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.text.x = element_text(face = "bold"),
+      axis.title = element_text(size = 12, face = "bold"),
+      strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
+      strip.text.y.left = element_text(angle = 0)
+    ) +
+    scale_colour_discrete("Model")
+  if(x$resptype == "survival") {
+    forestplot + xlab("Hazard ratio") +
+      geom_vline(xintercept = 1, linetype = 2)
   } else if (x$resptype == "binary") {
-    forestplot <- ggplot(
-      data = data,
-      aes(x = trt.estimate, y = model, xmin = trt.low, xmax = trt.high)
-    ) +
-      ggtitle("Forest plot Horseshoe") +
-      geom_pointrange(aes(col = model)) +
-      geom_vline(xintercept = 0, linetype = 2) +
-      ylab("Subgroup") +
-      xlab("Log Odds-Ratio") +
-      geom_errorbar(aes(xmin = trt.low, xmax = trt.high, col = model), width = 0.5, cex = 1) +
-      facet_wrap(~ forcats::fct_inorder(subgroup),
-        strip.position = "left",
-        nrow = nrow(data), scales = "free_y"
-      ) +
-      theme(
-        plot.title = element_text(size = 16, face = "bold"),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        strip.text.x = element_text(hjust = 0, vjust = 1, angle = 180, face = "bold"),
-        strip.text.y.left = element_text(angle = 0)
-      ) +
-      scale_colour_discrete("Model")
+    forestplot + xlab("Log Odds-Ratio") +
+      geom_vline(xintercept = 0, linetype = 2)
   }
-  forestplot
 }
