e83361acf48e907477956ebefcf2b997075454c1
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2024-03-26 14:25:58 +0100

    clean up notes

--- R/generate_stacked_data.R
+++ R/generate_stacked_data.R
@@ -47,7 +47,7 @@ generate_stacked_data <- function(base_model, subgroup_model, data,
     data,
     names_to = "subgroup_var",
     values_to = "subgroup_value",
-    -any_of(drop_vars)
+    -tidyselect::any_of(drop_vars)
   )
   subgroup <- paste(d$subgroup_var, d$subgroup_value, sep = ".")
   subgroup <- factor(subgroup, levels = subgroup_names)
@@ -57,13 +57,13 @@ generate_stacked_data <- function(base_model, subgroup_model, data,
     dplyr::rename_with(
       d,
       ~ c("arm", "time", "status"),
-      all_of(c(arm_var, resp_var, status_var))
+      tidyselect::all_of(c(arm_var, resp_var, status_var))
     )
   } else if (resptype == "binary") {
     dplyr::rename_with(
       d,
       ~ c("arm", "y"),
-      all_of(c(arm_var, resp_var))
+      tidyselect::all_of(c(arm_var, resp_var))
     )
   }
   d
