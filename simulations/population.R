7c28a9db5596306fa4700837b57a881c411f50cd
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2024-04-09 11:06:00 +0200

    format population results

--- simulations/population.R
+++ simulations/population.R
@@ -1,5 +1,16 @@
+# We still need to have all subgroups represented
+# in the population results object.
+all_subgroups <- c(
+  "S_1a", "S_1b", "S_2a", "S_2b", "S_3a", "S_3b", "S_4a", "S_4b",
+  "S_4c", "S_5a", "S_5b", "S_5c", "S_5d", "S_6a", "S_6b", "S_7a",
+  "S_7b", "S_8a", "S_8b", "S_8c", "S_9a", "S_9b", "S_10a", "S_10b",
+  "S_10c"
+)
+
 # Method for a single data set.
-population_method <- function(df) {
+population_method <- function(df, simul_no) {
+  assert_data_frame(df)
+  assert_count(simul_no)
   df$arm <- factor(df$arm)
   model <- naivepop(
     resp = "tt_pfs",
@@ -8,30 +19,25 @@ population_method <- function(df) {
     resptype = "survival",
     status = "ev_pfs"
   )
-  s <- summary(model)
+  ahr <- as.numeric(summary(model)$estimates)
   conf_int <- exp(confint(model$fit))
-  c(
-    hr = as.numeric(s$estimates),
-    lower = conf_int[1L],
-    upper = conf_int[2L]
+  data.frame(
+    simul_no = simul_no,
+    estimator = "population",
+    subgroup = all_subgroups,
+    estimate_ahr = ahr,
+    estimate_log_ahr = log(ahr),
+    lower_ci_ahr = conf_int[1L],
+    upper_ci_ahr = conf_int[2L]
   )
 }
 
 # Analysis of a single scenario.
-population_analysis <- function(scenario) {
-  assert_list(scenario)
-  results <- t(sapply(scenario, population_method))
-}
+population_analysis <- fun_analysis(population_method)
 
 # Results across all scenarios.
-population_file <- "results/population.rds"
-population_results <- if (file.exists(population_file)) {
-  readRDS(population_file)
-} else {
-  res <- lapply(
-    scenarios,
-    function(x) population_analysis(x$scenario)
-  )
-  saveRDS(res, file = population_file)
-  res
-}
+population_results <- compute_results(
+  scenarios,
+  analyze = population_analysis,
+  cache = "results/population.rds"
+)
