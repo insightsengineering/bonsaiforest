13b8907dcbdd4f1086ca3e5abb74e7e6acd824c9
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2024-06-05 13:58:37 +0200

    save scenarios as rds instead of RData

--- simulations/scenarios.R
+++ simulations/scenarios.R
@@ -9,8 +9,8 @@ out_dir <- "scenarios"
 set.seed(0)
 
 for (i in as.character(1:6)) {
-  tmp <- simul_scenario(i)
+  scen <- simul_scenario(i)
   scen_name <- paste0("scenario", i)
-  assign(scen_name, tmp)
-  save(list = scen_name, file = file.path(out_dir, paste0(scen_name, ".RData")))
+  scen_file <- file.path(out_dir, paste0(scen_name, ".rds"))
+  saveRDS(scen, file = scen_file)
 }
